0d80d5b1cc63da1ced97cb8244443d91f5ec6de5
Venushki Thilakawardana <thilakawardanavn.20@uom.lk>
2024-10-10 14:01:06 +0530

    navigation bar and background

--- script.js
+++ script.js
@@ -121,6 +121,10 @@ document.addEventListener("DOMContentLoaded", function () {
       validateField(input); // Validate field when focus is lost
     });
   });
+  document.getElementById("hamburger").addEventListener("click", function () {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+  });
 
   // Function to show the modal
   function showModal(message) {
